f74b37573d89eb9364877ccdc2b2bfa403a52c3d
kakaviboy291 <botdeploy365@gmail.com>
2024-08-01 09:54:00 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "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"
 const BOT_NUMBER = "94717267602"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~yT43kBbQ#W7VeratbfB4vczJbfmknHmgDLXztr6KfbGBGxf1ufnw"
 
 
 module.exports = {
